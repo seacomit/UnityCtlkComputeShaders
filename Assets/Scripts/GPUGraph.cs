a152c867e7b64730b4b4dfce525a7fad01610465
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-26 16:45:56 -0400

    Start drawing the instanced meshes without positions. Performance is not good, seems like it has to do with Unity Version.

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -26,6 +26,21 @@ public class GPUGraph : MonoBehaviour
 
     ComputeBuffer positionsBuffer;
 
+    [SerializeField]
+    ComputeShader computeShader;
+
+    [SerializeField]
+    Material material;
+
+    [SerializeField]
+    Mesh mesh;
+
+    static readonly int 
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+        stepId = Shader.PropertyToID("_Step"),
+        timeId = Shader.PropertyToID("_UnityTime");
+
     void OnEnable()
     {
         positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
@@ -35,6 +50,19 @@ public class GPUGraph : MonoBehaviour
     {
         positionsBuffer.Release();
         positionsBuffer = null;
+    }
+
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
     }
 
     void Update()
@@ -55,6 +83,8 @@ public class GPUGraph : MonoBehaviour
             transitionFunction = function;
             PickNextFunction();
         }
+
+        UpdateFunctionOnGPU();
     }
 
     void PickNextFunction()
